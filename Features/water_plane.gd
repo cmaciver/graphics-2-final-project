25f13a2f764adc12a66fdd30079af376cd1a4f7a
Adam Imbert <prismly55@gmail.com>
2025-04-29 20:09:46 -0400

    Remove Dead Scripts, Water Update
    - Made water bigger and hopefully less noticeably tiled

--- Features/water_plane.gd
+++
@@ -1,6 +0,0 @@
-class_name WaterPlane
-extends MeshInstance3D
-
-func _process(delta):
-	mesh.material.set_shader_parameter("movement_direction", RenderGlobals.wind_direction)
-	mesh.material.set_shader_parameter("movement_strength", RenderGlobals.wind_strength)
