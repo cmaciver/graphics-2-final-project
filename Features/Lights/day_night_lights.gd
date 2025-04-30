db4e7616789a9ae08a7324ad82a3d3977d51cb08
Camden MacIver <camdenmac@icloud.com>
2025-04-30 01:01:32 -0400

    touched a whole lot of stuff sorry not sorry

--- Features/Lights/day_night_lights.gd
+++ Features/Lights/day_night_lights.gd
@@ -1,8 +1,12 @@
 @icon("res://_ico/DirectionalLight3D.svg")
 extends Node3D
 
-@export var start_time = 6.0
-@export var hours_per_second = 1.0
+@export var start_time = 6.0 #:
+	#set(value):
+		#start_time = value
+		#$AnimationPlayer.seek(start_time)
+		
+@export var hours_per_second = 0.4
 
 func _ready():
 	$AnimationPlayer.speed_scale = hours_per_second
